c7cef487a0bc660ce32f9f9c6a3cfb22d0ad6363
AGARWALMANU3008 <manuagarwal3008@gmail.com>
2024-07-28 09:59:37 +0530

    navscroll

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Stack, Flex, Heading, Spacer, HStack, Button, useDisclosure, Box } from "@chakra-ui/react";
 import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 import ContactUsModal from '../ContactUsModal'; // Ensure this import is correct
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
     const handleTextClick = () => {
         window.location.reload(); // Reload the page when text is clicked
     };
 
     const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra UI hook for modal
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <Stack
-                p={5}
-                bg={'#1D1D1D'}
+                p={isScrolled ? 7 : 5}
+                height={isScrolled ? '100px' : '80px'}
+                bg={isScrolled ? 'rgba(29, 29, 29, 0.9)' : '#1D1D1D'}
                 as='header'
+                position='fixed'
+                width='100%'
+                top='0'
+                zIndex='10'
+                transition='all 0.3s ease'
             >
                 <Flex
                     w='full'
@@ -52,7 +75,7 @@ const Navbar = () => {
                     </HStack>
                 </Flex>
             </Stack>
-            <Box id="main-content">
+            <Box id="main-content" pt="80px">
                 {/* Your main content here */}
             </Box>
             <ContactUsModal isOpen={isOpen} onClose={onClose} />
